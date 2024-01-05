f388d02552f55bcda6a4a46dcdb930b977a706e8
wallace oliveira <78317278+wallace14k@users.noreply.github.com>
2024-01-05 10:26:31 -0300

    Criação da solução

---
+++ PolimofismoExemplo/PolimofismoExemplo/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var numero = new Numero();
+
+numero.num = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine(numero.num + "-" + numero.tipo);
+
+
+public class ValidarNumero
+{
+    public virtual string Tipo(int numero)
+    {
+        if (numero % 2 == 0)
+        {
+            return "Par";
+        }
+        else
+        {
+            return "Impar";
+        }
+    }
+}
+
+
+public class Numero : ValidarNumero
+
+{
+    public int num { get; set; }
+    public string tipo { get => Tipo(num); }
+    public override string Tipo(int numero)
+    {
+        return base.Tipo(numero);
+    }
+}
+
+
+
+
